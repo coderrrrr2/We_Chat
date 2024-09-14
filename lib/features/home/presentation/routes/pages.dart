effdd97927356601b43a0d25d473a13b9c5d8046
Adebayo Philip <114924407+coderrrrr2@users.noreply.github.com>
2024-09-14 11:30:07 +0100

    working on chat message implementation

--- lib/features/home/presentation/routes/pages.dart
+++ lib/features/home/presentation/routes/pages.dart
@@ -1,5 +1,6 @@
-import 'package:get/get_navigation/src/routes/get_route.dart';
-import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
 import 'package:practice_chat_app/features/home/presentation/routes/routes.dart';
 import 'package:practice_chat_app/features/home/presentation/view/chat_page.dart';
 import 'package:practice_chat_app/features/home/presentation/view/home_view.dart';
@@ -16,7 +17,9 @@ List<GetPage> homePages = [
       ]),
   GetPage(
     name: HomeRoutes.chatView,
-    page: () => const ChatPage(),
+    page: () => ChatPage(
+      args: Get.arguments as ChatPageArgs,
+    ),
     transition: Transition.native,
     transitionDuration: const Duration(milliseconds: 500),
   ),
--- lib/features/home/presentation/view/chat_page.dart
+++ lib/features/home/presentation/view/chat_page.dart
@@ -1,15 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
+import 'package:practice_chat_app/core/init/init_services.dart';
+import 'package:practice_chat_app/features/auth/viewmodel/provider_list.dart';
+import 'package:practice_chat_app/features/home/data/enums.dart';
+import 'package:practice_chat_app/models/chat_model.dart';
+import 'package:practice_chat_app/models/message_model.dart';
+import 'package:practice_chat_app/models/user_model.dart';
+
+class ChatPageArgs {
+  final UserProfile userProfile;
+  ChatPageArgs({required this.userProfile});
+}
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  final ChatPageArgs args;
+  const ChatPage({super.key, required this.args});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  ChatUser? currentUser, otherUser;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = ChatUser(
+        id: authService.user!.uid, firstName: authService.user!.displayName);
+    otherUser = ChatUser(
+        id: widget.args.userProfile.uid,
+        firstName: widget.args.userProfile.name,
+        profileImage: widget.args.userProfile.pfpUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(widget.args.userProfile.name),
+      ),
+      body: StreamBuilder(
+          stream: dataBaseService.getMessages(currentUser!.id, otherUser!.id),
+          builder: (context, snapshot) {
+            Chat? chat = snapshot.data.data();
+            final List<ChatMessage> messages = [];
+            return DashChat(
+                messageOptions:
+                    MessageOptions(showCurrentUserAvatar: true, showTime: true),
+                inputOptions: InputOptions(alwaysShowSend: true),
+                currentUser: currentUser!,
+                onSend: (message) {
+                  sendMessage(message);
+                },
+                messages: messages);
+          }),
+    );
+  }
+
+  Future<void> sendMessage(ChatMessage messageTyped) async {
+    Message message = Message(
+        senderId: currentUser!.id,
+        content: messageTyped.text,
+        sentAt: Timestamp.fromDate(messageTyped.createdAt),
+        messageType: MessageType.text);
+    await dataBaseService.sendChatMessage(
+        currentUser!.id, otherUser!.id, message);
   }
 }
